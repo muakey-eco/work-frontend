cf51389eae24c2fc6e3a84c711f91ac9a5a1c2ef
Kanghocit <ankhangit06@gmail.com>
2025-05-24 17:37:57 +0700

    fix: delete report field

--- src/libs/statistics.ts
+++ src/libs/statistics.ts
@@ -1,10 +1,5 @@
 import { requestWithAuthorized } from './request'
 
-export const getReportStatistics = async (query?: any) =>
-  requestWithAuthorized('general-report?' + new URLSearchParams(query))
-    .then((data) => data)
-    .catch(() => [])
-
 export const getSchedule = async (query?: any) =>
   requestWithAuthorized('schedule?' + new URLSearchParams(query))
     .then((data) => data)
